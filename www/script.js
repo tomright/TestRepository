53a47ff16ee830030f0e8c1724073f404ae27afa
Azat Rakipov <9uvc@mail.ru>
2022-07-16 18:20:46 +0500

    First commit. Создал первую консольную программу на JS

---
+++ www/script.js
@@ -0,0 +1,19 @@
+'use strict';
+
+let numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели', '');
+
+const personalMoveDB = {
+    count: numberOfFilms,
+    moves: {},
+    actors: {},
+    genres: [],
+    private: false
+};
+
+const lastFilm = prompt('Один из последних просмотренных фильмов?', '');
+const lastFilmRating = prompt('На сколько оцените последний фильм?', '');
+
+personalMoveDB.moves[lastFilm] = lastFilmRating;
+
+console.log(personalMoveDB);
+
